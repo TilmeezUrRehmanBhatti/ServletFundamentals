5d50ae5559b2a5ba6260217564b3d5161481b2d3
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-08-06 12:39:05 +0200

    initial comment

--- src/main/java/com/tilmeez/servletdemo/HelloWorldServlet.java
+++ src/main/java/com/tilmeez/servletdemo/HelloWorldServlet.java
@@ -1,14 +1,30 @@
+package com.tilmeez.servletdemo;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", value = "/HelloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // Step 1: set the content type
+        response.setContentType("text/html");
+
+        // Step 2: get the printwriter
+        PrintWriter out = response.getWriter();
+
+        // Step 3: generate HTML content
+        out.println("<html><body>");
+
+        out.println("<h2>Hello World</h2>");
+        out.println("<hr>");
+        out.println("Time on the server is:" + new java.util.Date());
+        
+        out.println("</body></html>");
     }
 
     @Override
